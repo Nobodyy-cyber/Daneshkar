0ae22602d09d47072ebce8452506a9f7142fccb7
Nobodyy-cyber <aminshafiei34@gmail.com>
2026-06-21 11:12:03 -0500

    Debuging 06

--- 03-Docker/06_docker_compose/compose_inspect.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# cd 06_docker_compose
-docker compose up -d
-sleep 5
-docker compose exec web ping -c 2 app &> service_communication.txt
-docker network ls | grep docker_compose &> compose_networks.txt
-docker volume ls | grep docker_compose &> compose_volumes.txt
-docker compose down -v
--- 03-Docker/06_docker_compose/env_test.sh
+++ 03-Docker/06_docker_compose/env_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker compose config &> compose_with_env.txt
-docker compose up -d
+docker-compose config > compose_with_env.txt
+docker-compose up -d
 sleep 5
-docker compose exec db env | grep MYSQL &> db_env_vars.txt
-docker compose down
+docker-compose exec db env | grep MYSQL > db_env_vars.txt
+docker-compose down
